05ad94fc6b9ac5cf0ed9b5ec5a34cc266a2892d8
wooyong99 <wulovesk@naver.com>
2023-02-25 09:51:03 +0900

    중복되는 지역변수 전역변수로 전환

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,19 +9,19 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editTextNum1 ;
+    EditText editTextNum2 ;
+    TextView textView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextNum1 = findViewById(R.id.activity_main__editTextNum1);
+        editTextNum2 = findViewById(R.id.activity_main__editTextNum2);
+        textView = findViewById(R.id.activity_main__textView);
     }
 
     public void btnPlus(View view) {
-//        findViewById 메소드를 통해서 View에있는 요소를 불러옴
-        EditText editTextNum1 = findViewById(R.id.activity_main__editTextNum1);
-        EditText editTextNum2 = findViewById(R.id.activity_main__editTextNum2);
-        TextView textView = findViewById(R.id.activity_main__textView);
-
 //        요소의 값이 공백이 있는 경우를 대비하여 공백제거 코드
         editTextNum1.setText(editTextNum1.getText().toString().trim());
         editTextNum2.setText(editTextNum2.getText().toString().trim());
@@ -29,10 +29,12 @@ public class MainActivity extends AppCompatActivity {
 //        공백 제거한 요소 값의 길이가 0일 경우 코드 구현
         if( editTextNum1.getText().toString().length() == 0 ){
             Toast.makeText(getApplicationContext(),"숫자를 입력해주세요.", Toast.LENGTH_SHORT).show();
+            editTextNum1.requestFocus();
             return ;
         }
         if( editTextNum2.getText().toString().length() == 0 ){
             Toast.makeText(getApplicationContext(), "숫자를 입력해주세요.", Toast.LENGTH_SHORT).show();
+            editTextNum2.requestFocus();
             return ;
         }
 
@@ -47,10 +49,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnClear(View view) {
-        EditText editTextNum1 = findViewById(R.id.activity_main__editTextNum1);
-        EditText editTextNum2 = findViewById(R.id.activity_main__editTextNum2);
-        TextView textView = findViewById(R.id.activity_main__textView);
-
 //        모든 요소의 text 값을 초기화
         editTextNum1.setText("");
         editTextNum2.setText("");
@@ -58,6 +56,28 @@ public class MainActivity extends AppCompatActivity {
 
 //        하단 메시지 알림 표시
         Toast.makeText(getApplicationContext(), "초기화 되었습니다.", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void btnMinus(View view) {
+        editTextNum1.setText(editTextNum1.getText().toString().trim());
+        editTextNum2.setText(editTextNum2.getText().toString().trim());
+
+        if( editTextNum1.getText().toString().length() == 0 ){
+            Toast.makeText(getApplicationContext(),"숫자를 입력해주세요.", Toast.LENGTH_SHORT).show();
+            editTextNum1.requestFocus();
+            return ;
+        }
+        if( editTextNum2.getText().toString().length() == 0 ){
+            Toast.makeText(getApplicationContext(), "숫자를 입력해주세요.", Toast.LENGTH_SHORT).show();
+            editTextNum2.requestFocus();
+            return ;
+        }
+
+        int num1 = Integer.parseInt(editTextNum1.getText().toString());
+        int num2 = Integer.parseInt(editTextNum2.getText().toString());
+        int rs = num1 - num2;
+        textView.setText("결과 : " + rs);
 
     }
 }
